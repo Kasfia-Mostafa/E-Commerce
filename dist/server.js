e3d6d626ba1e44553efda5290fb67c4283484bc6
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2024-05-26 12:59:46 +0600

    Installation of Ts-Node-Dev

--- dist/server.js
+++ dist/server.js
@@ -19,8 +19,8 @@ function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             yield mongoose_1.default.connect(config_1.default.database_url);
-            app_1.default.listen(port, () => {
-                console.log(`E-Commerce app listening on port ${port}`);
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`E-Commerce app listening on port ${config_1.default.port}`);
             });
         }
         catch (err) {
--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,10 @@ async function main() {
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`App listening on port ${config.port}`);
+      console.log(`E-Commerce app listening on port ${config.port}`);
     });
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
 main();
