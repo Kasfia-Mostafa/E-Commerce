6563e17602b606d958e11c3cf6b00e3876508501
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2024-05-26 11:57:46 +0600

    Installation of  Eslint

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import config from "./app/config";
-import mongoose from "mongoose";
-import app from "./app";
+import config from './app/config';
+import mongoose from 'mongoose';
+import app from './app';
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(port, () => {
-      console.log(`E-Commerce app listening on port ${port}`);
+    app.listen(config.port, () => {
+      console.log(`App listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
