951a1290edcb59ec41f8cf59d696cf99869252ee
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2024-05-28 11:40:24 +0600

    Deployment in vercel

--- dist/app.js
+++ dist/app.js
@@ -8,16 +8,18 @@ const cors_1 = __importDefault(require("cors"));
 const phones_route_1 = require("./Products/modules/phones/phones.route");
 const orders_route_1 = require("./Orders/modules/orders/orders.route");
 const app = (0, express_1.default)();
-// parser
+// Middleware
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-// application routes
+// Default route
+app.get('/', (req, res) => {
+    res.send('Welcome to the E-Commerce');
+});
+// Application routes
 app.use('/api/products', phones_route_1.PhonesRoutes);
 app.use('/api/orders', orders_route_1.OrdersRoutes);
+// 404 handler
 app.use((req, res) => {
     res.status(404).json({ success: false, message: 'Route not found' });
-});
-app.get('/', (req, res) => {
-    res.send('Hello');
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -5,20 +5,22 @@ import { OrdersRoutes } from './Orders/modules/orders/orders.route';
 
 const app: Application = express();
 
-// parser
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-// application routes
+// Default route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to the E-Commerce');
+});
+
+// Application routes
 app.use('/api/products', PhonesRoutes);
 app.use('/api/orders', OrdersRoutes);
 
+// 404 handler
 app.use((req: Request, res: Response) => {
   res.status(404).json({ success: false, message: 'Route not found' });
-});
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello');
 });
 
 export default app;
